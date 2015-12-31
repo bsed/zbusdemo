9daed4993d2becfb54371e785f5fc22a15399ed3
d0f <284178359@qq.com>
2015-12-31 14:09:07 +0800

    磁盤隊列性能測試

--- src/main/java/cn/gnux/zbus/mq/diskq/DiskQueuePerf.java
+++ src/main/java/cn/gnux/zbus/mq/diskq/DiskQueuePerf.java
@@ -11,6 +11,9 @@ package cn.gnux.zbus.mq.diskq;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.zbus.mq.server.support.DiskQueuePool;
+import org.zbus.mq.server.support.DiskQueuePool.DiskQueue;
+
 /**
  * ClassName:DiskQueuePerf <br/>
  * Date:     2015年12月31日 上午9:12:41 <br/>
@@ -24,6 +27,45 @@ public class DiskQueuePerf {
 		int loopCount = 10000;
 		long startTime;
 		AtomicLong counter;
+		public void run() {
+			DiskQueue q = DiskQueuePool.getDiskQueue("test");
+			for(int i=0;i<loopCount;i++) {
+				try {
+					q.offer(new byte[1024]);
+					long count = counter.incrementAndGet();
+					if(count%20000==0) {
+						long end = System.currentTimeMillis();
+						System.out.format("QPS: %.2f\n", count*1000.0/(end-startTime));
+					}
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+		}
 		
+	}
+	
+	public static void main(String[] args) {
+		DiskQueuePool.init("C:\\MFQ");
+		final int loopCount = 10000000; 
+		final int threadCount = 16;
+		AtomicLong counter = new AtomicLong(0);
+		final long start = System.currentTimeMillis();
+		Task[] tasks = new Task[threadCount];
+		for(int i=0;i<tasks.length;i++) {
+			tasks[i] = new Task();
+			tasks[i].loopCount = loopCount;
+			tasks[i].startTime = start;
+			tasks[i].counter = counter;
+		}
+		
+		for(Task task :tasks) {
+			task.start();
+		}
+
+		DiskQueuePool.destory();
+		System.out.println("===done==="); 
 	}
 }
