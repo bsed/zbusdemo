f4902f3c9e74d640f3f960b20c06b5814f2facec
d0f <284178359@qq.com>
2015-12-31 14:09:45 +0800

    生產者性能測試

---
+++ src/main/java/cn/gnux/zbus/mq/diskq/ProducerPerf.java
@@ -0,0 +1,21 @@
+/**
+ * Project Name: zbusdemo
+ * File Name: ProducerPerf.java
+ * Package Name: cn.gnux.zbus.mq.diskq
+ * Date: 2015年12月31日下午2:06:47
+ * Copyright (c) 2015, kelvin@gnux.cn All Rights Reserved.
+ *
+*/
+
+package cn.gnux.zbus.mq.diskq;
+/**
+ * ClassName:ProducerPerf <br/>
+ * Date:     2015年12月31日 下午2:06:47 <br/>
+ * @author   lenovo
+ * @version  
+ * @since    JDK 1.7
+ * @see      
+ */
+public class ProducerPerf {
+
+}
